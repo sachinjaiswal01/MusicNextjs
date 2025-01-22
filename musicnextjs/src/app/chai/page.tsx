f454bce5d2e0042738278e850abb1f2be03d78b3
sachinjaiswal01 <jayswalsachin012@gmail.com>
2025-01-22 19:59:54 +0530

    Navbar using Acentrinity UI

--- musicnextjs/src/app/chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ChaiPage(){
-    return(
-        <h1 className="text-3xl text-center mt-8 text-orange-700"> Chai page</h1>
-    )
-}
--- musicnextjs/src/app/page.tsx
+++ musicnextjs/src/app/page.tsx
@@ -3,6 +3,9 @@
 
 export default function Home() {
   return (
-   <h1 className="text-2xl text-center bg-blend-color-dodge"> This is a heading</h1>
+    <main className= "min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center"> This is a   heading</h1>
+    </main>
+
   );
 }
