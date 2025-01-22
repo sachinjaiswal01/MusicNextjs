da22f42ecaadaa72b0b9dc4bcdb2384e5e46a63a
sachinjaiswal01 <jayswalsachin012@gmail.com>
2025-01-23 01:47:40 +0530

    Solving HeroSection

--- musicnextjs/src/app/page.tsx
+++ musicnextjs/src/app/page.tsx
@@ -1,10 +1,11 @@
 //page serve content
 // import Image from "next/image";
-
+import HeroSection from '@/components/HeroSection'
 export default function Home() {
   return (
     <main className= "min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center"> This is a   heading</h1>
+      
+      <HeroSection/>
     </main>
 
   );
