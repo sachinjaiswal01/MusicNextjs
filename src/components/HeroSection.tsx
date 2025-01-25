d4b068b5b0c1aa8d66aa59974be1de1d33c49402
sachinjaiswal01 <jayswalsachin012@gmail.com>
2025-01-25 20:56:22 +0530

    building featured card section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -28,9 +28,9 @@ function HeroSection() {
                 <Button
                      borderRadius="1.75rem"
                      className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                    >
-        Explore Courses
-      </Button>
+                >
+                    Explore Courses
+                </Button>
                 </Link>
             </div>
 
