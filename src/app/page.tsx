e9494ebedc656d6a5ac926d7149dc3b8a19d9697
sachinjaiswal01 <jayswalsachin012@gmail.com>
2025-01-27 15:14:52 +0530

    Grid background and Moving cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@
 // import Image from "next/image";
 import FeaturedCourses from '@/components/FeaturedCourses';
 import HeroSection from '@/components/HeroSection'
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import WhyChooseUs from '@/components/WhyChooseUs';
 export default function Home() {
   return (
     <main className= "min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
-      
+      <MusicSchoolTestimonials/>
     </main>
 
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "tailwindcss";
-const colors = require("tailwindcss/colors");
+
 const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
 
+const svgToDataUri = require("mini-svg-data-uri");
+ 
+const colors = require("tailwindcss/colors");
 // Plugin to add each Tailwind color as a global CSS variable
 function addVariablesForColors({ addBase, theme }: any) {
   const allColors = flattenColorPalette(theme("colors"));
@@ -25,6 +28,8 @@ const config: Config = {
     extend: {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       keyframes: {
         spotlight: {
@@ -37,6 +42,11 @@ const config: Config = {
             transform: "translate(-50%, -40%) scale(1)",
           },
         },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
       },
       colors: {
         background: "var(--background)",
@@ -44,7 +54,31 @@ const config: Config = {
       },
     },
   },
-  plugins: [addVariablesForColors],
+  plugins: [addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+    
+  ],
 };
 
 export default config;
