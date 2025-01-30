38e5b8676287477e91614400bd1dce1f16e67eb9
sachinjaiswal01 <jayswalsachin012@gmail.com>
2025-01-30 16:49:22 +0530

    restoreing deleted ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-//page serve content
-// import Image from "next/image";
-import FeaturedCourses from '@/components/FeaturedCourses';
-import HeroSection from '@/components/HeroSection'
-import Instructor from '@/components/Instructor';
+
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructor";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
-import UpcomingWebinars from '@/components/UpcomingWebinars';
-import WhyChooseUs from '@/components/WhyChooseUs';
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+
 export default function Home() {
   return (
-    <main className= "min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <HeroSection/>
-      <FeaturedCourses/>
-      <WhyChooseUs/>
-      <MusicSchoolTestimonials/>
-      <UpcomingWebinars/>
-      <Instructor/>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
-
   );
 }
