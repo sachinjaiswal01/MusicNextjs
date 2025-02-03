3d78ecfc6ba7aa2ecf094cbad0d22197b8849fd7
sachinjaiswal01 <jayswalsachin012@gmail.com>
2025-02-03 23:47:40 +0530

    type check

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -99,6 +99,7 @@ export const BackgroundBeamsWithCollision = ({
 
 const CollisionMechanism = React.forwardRef<
   HTMLDivElement,
+  
   {
     containerRef: React.RefObject<HTMLDivElement>;
     parentRef: React.RefObject<HTMLDivElement>;
@@ -114,7 +115,7 @@ const CollisionMechanism = React.forwardRef<
       repeatDelay?: number;
     };
   }
->(({ parentRef, containerRef, beamOptions = {} }) => {
+>(({ parentRef, containerRef, beamOptions = {} },ref) => {
   const beamRef = useRef<HTMLDivElement>(null);
   const [collision, setCollision] = useState<{
     detected: boolean;
--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -16,29 +16,27 @@ export const HoverEffect = ({
   return (
     <div className={cn("grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10", className)}>
       {items.map((item, idx) => (
-        <Link href={item.link} key={item.link} passHref>
-          <a
-            className="relative group block p-2 h-full w-full"
-            onMouseEnter={() => setHoveredIndex(idx)}
-            onMouseLeave={() => setHoveredIndex(null)}
-          >
-            <AnimatePresence>
-              {hoveredIndex === idx && (
-                <motion.span
-                  className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl"
-                  layoutId={`hoverBackground-${idx}`} // Unique layoutId
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1, transition: { duration: 0.15 } }}
-                  exit={{ opacity: 0, transition: { duration: 0.15, delay: 0.2 } }}
-                />
-              )}
-            </AnimatePresence>
-            <Card>
-              <CardTitle>{item.title}</CardTitle>
-              <CardDescription>{item.description}</CardDescription>
-            </Card>
-          </a>
-        </Link>
+       <Link href={item.link} key={item.link} className="relative group block p-2 h-full w-full" 
+       onMouseEnter={() => setHoveredIndex(idx)}
+       onMouseLeave={() => setHoveredIndex(null)}
+     >
+       <AnimatePresence>
+         {hoveredIndex === idx && (
+           <motion.span
+             className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl"
+             layoutId={`hoverBackground-${idx}`} // Unique layoutId
+             initial={{ opacity: 0 }}
+             animate={{ opacity: 1, transition: { duration: 0.15 } }}
+             exit={{ opacity: 0, transition: { duration: 0.15, delay: 0.2 } }}
+           />
+         )}
+       </AnimatePresence>
+       <Card>
+         <CardTitle>{item.title}</CardTitle>
+         <CardDescription>{item.description}</CardDescription>
+       </Card>
+     </Link>
+     
       ))}
     </div>
   );
